76cc0db151f227bdfc0cae575f2d03f0d0d393ac
Shashank <shashank96.nayak@gmail.com>
2021-03-27 22:31:22 +0530

    Created welcome component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { loginComponent } from "./login/login.component";
 import { signupComponent } from "./signup/signup.component";
+import { welcomeComponent } from "./welcome/welcome.component";
 
 const routes: Routes = [
+    {path: '', component: welcomeComponent},
     {path: 'login', component: loginComponent},
     {path: 'signup', component: signupComponent}
 ]
