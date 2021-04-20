3a31f573b7f1d30b0be26f64db8dfb5ada6f4b2d
Arya Bhagwat <aryabhagwat1@gmail.com>
2021-04-21 01:45:16 +0530

    updated the router method to delete

--- backend/AdminRoutes/admin.js
+++ backend/AdminRoutes/admin.js
@@ -24,7 +24,7 @@ router.get('/getMovies', adminController.getMovies);
 
 router.get('/movies/:movieId', adminController.getMovieByID); 
 
-router.get('/deletemovie/:movieId', adminController.deleteMovieByID); 
+router.delete('/deletemovie/:movieId', adminController.deleteMovieByID); 
 
 console.log("after login");
 module.exports = router;
